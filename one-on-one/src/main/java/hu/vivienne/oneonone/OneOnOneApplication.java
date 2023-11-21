3322cbaa227476b250768b20276326b5bd62020c
Boda <bodavivienviktoria@gmail.com>
2023-11-21 15:04:15 +0100

    database setup

---
+++ one-on-one/src/main/java/hu/vivienne/oneonone/OneOnOneApplication.java
@@ -0,0 +1,13 @@
+package hu.vivienne.oneonone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneOnOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneOnOneApplication.class, args);
+	}
+
+}
---
+++ one-on-one/src/test/java/hu/vivienne/oneonone/OneOnOneApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.vivienne.oneonone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneOnOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
